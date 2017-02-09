1c8771f8fb58fd482ae355cbbc1a5be591458f61
Valery_Bilyi <Valery_Bilyi@EPUAKYIW2962>
2017-02-09 19:12:31 +0200

    Added adaptive header

---
+++ PayDay/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    //Scroll button
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() != 0) {
+            $(".to-top").fadeIn();
+        } else {
+            $(".to-top").fadeOut();
+        }
+    });
+
+    $(".to-top").click(function () {
+        $('body,html').animate({ scrollTop: 0 }, 800);
+    });
+    /////////
+//MOBILE-menu button
+    $(".fa-bars").click(function () {
+        $(".header .nav-menu").toggle(300);
+    });
+    ///
+});
